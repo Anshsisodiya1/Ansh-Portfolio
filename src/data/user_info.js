571551b9512362e7a087abc84ad21f37de096081
Ansh Sisodiya <sisodiyaansh785@gmail.com>
2024-11-27 09:06:22 -0700

    Added my project

---
+++ src/data/user_info.js
@@ -0,0 +1,179 @@
+const info = {
+    // ============ MAIN DETAILS ============
+    main: {
+        name: "Ansh Sisodiya",
+        description: "I am a dedicated software engineer and a BCA 2nd-year student, specializing in full-stack web development. With a strong foundation in building scalable, secure, and reliable web applications, I bring creativity and technical precision to every project I undertake.. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards.",
+        role: "Full-Stack Developer",
+        photo: "../ansh.webp",
+        email: "sidoiyaansh785@gmail.com",
+    },
+
+    // ============ SOCIAL LINKS ============
+    socials: {
+        twitter: "https://x.com/Anshsisodiya57",
+		// github: "https://github.com/dsbalico",
+		linkedin: "https://www.linkedin.com/in/ansh-sisodiya-842001313?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+		instagram: "https://www.instagram.com/bittuthakur5257/",
+    },
+
+    // ============ PROJECTS ============
+    projects: [
+        {
+			title: "React Blog-Site",
+			description: "Developed a fully responsive blog website using React.js, featuring dynamic content rendering and an engaging user interface.",
+            technologies: "React js, Css, JavaScript",
+			github: "https://github.com/Anshsisodiya1/React_Blog-site.git",
+            link: "https://ansh-react-blog.netlify.app"
+		},
+
+        {
+			title: "Password Generator",
+			description: "This project allows users to generate strong, with options to include uppercase letters, lowercase letters, numbers, and special characters.",
+			technologies: "React js, Css, JavaScript",
+			github: "https://github.com/Anshsisodiya1/Password_Generator.git",
+            link: "https://github.com/dsbalico/YouShare"
+		},
+
+        {
+			title: "To do List",
+			description: "This project enables users to add, edit, and delete tasks, helping them organize and prioritize their daily activities efficiently.",
+			technologies: "React js, Css, JavaScript",
+			github: "https://github.com/Anshsisodiya1/React-todo.git",
+            link: "https://ansh-react-todo.netlify.app"
+		},
+
+		{
+			title: "Diital Clock",
+			description: "Digital Clock is created using HTML, CSS, and JavaScript.This project displays the current time dynamically, updating in real-time with an elegant design.",
+			technologies: "HTML, Css, JavaScript",
+			github: "https://github.com/Anshsisodiya1/Digital-Clock.git",
+            link: " https://anshsisodiya1.github.io/Digital-Clock/"
+		},
+
+		{
+			title: "Sudoku Solver",
+			description: "Sudoku Solver is a web application that can generate or solve sudoku problems using simulated annealing, an optimization algorithm.",
+			technologies: "React, Tailwind, TypeScript",
+			github: "https://github.com/dsbalico/sudoku-solver-using-simulated-annealing",
+            link: "https://sudoku-solver-sa.netlify.app/"
+		},
+
+		{
+			title: "Probability Pick",
+			description: "Probability Pick is a PCSO Lotto Number Generator that calculates the likelihood of selecting winning numbers based on historical draw data.",
+			technologies: "React, Tailwind, Flask",
+			github: "https://github.com/dsbalico/pcso-lotto-number-generator",
+            link: "https://probability-pick.netlify.app/"
+		},
+    ],
+
+    // ============ EDUCATION ============
+    education: [
+        {
+            school: "Renaissance University Indore, India",
+            degree: "Bachelor in Computer Science",
+            duration: "2023 - Present",
+            image: "Ru.webp"
+        },
+        {
+            school: "College Dekho",
+            degree: "Full stack Development",
+            duration: "2023 - Present",
+            image: "CD.webp"
+        },
+    ],
+
+    // ============ EXPERIENCE ============
+    experience: [
+        {
+            position: " ",
+            company: " ",
+            duration: " ",
+            image: "ccci.webp",
+            descriptions: [
+                " ",
+                " ",
+                " ",
+            ]
+        },
+        {
+            position: " ",
+            company: " ",
+            duration: " ",
+            image: " ",
+            descriptions: [
+                " ",
+                " "
+            ]
+        },
+    ],
+
+    // ============ CERTIFICATES ============
+    certificates: [
+        {
+            title: " ",
+            description: " ",
+            icon: " ",
+            link: " ",
+        },
+        {
+            title: " ",
+            description: " ",
+            icon: " ",
+            link: " ",
+        },
+        {
+            title: " ",
+            description: " ",
+            icon: " ",
+            link: " " ,
+        },
+        {
+            title: " ",
+            description: " ",
+            icon: " ",
+            link: " ",
+        },
+        {
+            title: " ",
+            description: " ",
+            icon: " ",
+            link: " "
+        },
+        {
+            title: " ",
+            description: " ",
+            icon: " ",
+            link: " "
+        },
+        {
+            title: " ",
+            description: " ",
+            icon: " ",
+            link: " "
+        },
+        {
+            title: " ",
+            description: " ",
+            icon: " ",
+            link: " "
+        },
+        {
+            title: " ",
+            description: " ",
+            icon: " ",
+            link: " "
+        }
+    ],
+
+    // ============ CONTACT ============
+    contact: {
+        title: "Let's Get in Touch: Ways to Connect with Me",
+        description: "Thank you for your interest in getting in touch with me. I welcome your feedback, questions, and suggestions. If you have a specific question or comment, please feel free to email me directly at danielshan.balico@gmail.com. I make an effort to respond to all messages within 24 hours, although it may take me longer during busy periods. Finally, if you prefer to connect on social media, you can connect with me using the social media links below.",
+    },
+
+    // ============ FOOTER ============
+    footer: "© 2024 Ansh Sisodiya. All Rights Reserved"
+}
+
+export default info
